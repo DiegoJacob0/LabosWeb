eaf1252961c1a52878cc687753d453b817886156
Diego Jacobo <00043719@uca.edu.svgit config --global user.name DiegoJacob0>
2025-11-08 12:13:53 -0600

    subi archivo que no era😭

--- 00043719_practica8_seccion1/Fronted/laboratorio/src/App.jsx
+++ 00043719_practica8_seccion1/Fronted/laboratorio/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
@@ -7,8 +6,8 @@ import Protected from "./components/Protected";
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Protected" element={<Protected />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/protected" element={<Protected />} />
     </Routes>
   </Router>
 );
